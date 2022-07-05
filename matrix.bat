a99474404272ed6a279411ef1be9f1ddf9e09957
BLACK-DEVIL-8212 <75551857+BLACK-DEVIL-8212@users.noreply.github.com>
2022-07-05 09:20:01 +0545

    Create matrix.bat

---
+++ matrix.bat
@@ -0,0 +1,5 @@
+@echo off
+:top
+color a 
+echo %ranodm% %random% %ranodm% %random% %ranodm% %random% %ranodm% %random% %ranodm% %random% %ranodm% %random% %ranodm% %random% %ranodm% %random% %ranodm% %random%
+goto top
