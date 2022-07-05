b87ee5f9f383b38ee45ee34fb3c09c906b7edb30
BLACK-DEVIL-8212 <75551857+BLACK-DEVIL-8212@users.noreply.github.com>
2022-07-05 09:25:53 +0545

    Create upload.py

---
+++ upload.py
@@ -0,0 +1,11 @@
+import time
+
+print("[+] stating program ...")
+time.sleep(1)
+print("[+] service started ...")
+time.sleep(5)
+f1 = open('matrix.bat',"w+')
+f2 = open("disable-internet.bat","w+")
+          
+a = ["fi","f2]
+a.open();
